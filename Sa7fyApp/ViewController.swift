fe434e6fc5fac9d8f59147a8e3395ed4e4647cce
Mamdouh El Nakeeb <mamdouh..el.nakeeb@gmail.com>
2018-07-03 00:01:29 +0200

    Initial Commit

---
+++ Sa7fyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sa7fyApp
+//
+//  Created by Mamdouh El Nakeeb on 7/3/18.
+//  Copyright © 2018 Mamdouh El Nakeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
